e5ad54991db694abefddc8ae3e7e58c06ddfa573
PHANAGUS <phanarin03072002@gmail.com>
2022-08-04 14:17:03 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const icon = document.getElementById("span-gender-icon");
+const userCard = document.getElementById("div-user-card");
+const loadCard = document.getElementById("div-loading-card");
+const pimg = document.getElementById("img-profile");
+const pname = document.getElementById("p-name");
+const pemail = document.getElementById("p-email");
+const paddress = document.getElementById("p-address");
+
+// const axios = require("axios");
 async function callApi() {
-  const resp = await axios.get("https://randomuser.me/api/");
+  try {
+    const resp = await axios.get("https://randomuser.me/api/");
+    const user = resp.data.results[0];
+    if (user.name.title === "Mr") icon.innerText = "👨";
+    else icon.innerText = "👩";
+    pname.innerText = user.name.first + " " + user.name.last;
+    pemail.innerText = user.email;
+    paddress.innerText = user.address;
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+callApi();
+
+userCard.style.display = "";
